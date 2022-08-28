14a0f4df8cb26fa5d7952133019c6cce032cb167
hkalkhor_elcomp <ah.zeinali94@gmail.com>
2022-08-28 14:32:51 -0700

    Update test_jax.py

--- test_jax.py
+++ test_jax.py
@@ -2,7 +2,7 @@ from jax import random
 import time
 import numpy as np
 
-N = 4096
+N = 8192
 
 key = random.PRNGKey(0)
 
@@ -10,12 +10,15 @@ key = random.PRNGKey(0)
 A = random.normal(key, shape=(N,N), dtype=np.float32)
 # N^2
 B = random.normal(key, shape=(N,N), dtype=np.float32)
-
-for i in range(100):
+print('\nGPU/TPU testing: ')
+SUM = 0
+iterations = 100
+for i in range(iterations):
     flop = 2*N*N*N
     start = time.monotonic()
     # N^2
     C = A @ B
     end = time.monotonic()
     flops = flop / (end - start)
-    print(f"{flops / 1e9:,.2f} GFLOPS")
+    SUM += flops / 1e12
+print(f"average: {SUM/iterations:,.2f} TFLOPS")
