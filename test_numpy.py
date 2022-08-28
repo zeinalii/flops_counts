a0acecc98a931eda8e79bc875fbe750dc632f6a9
hkalkhor_elcomp <ah.zeinali94@gmail.com>
2022-08-28 14:32:53 -0700

    Update test_numpy.py

--- test_numpy.py
+++ test_numpy.py
@@ -2,7 +2,7 @@ import numpy as np
 import time
 
 
-N = 4096
+N = 2**14
 
 np.random.seed(0)
 
@@ -10,14 +10,16 @@ np.random.seed(0)
 A = np.random.rand(N, N).astype(np.float32)
 # N^2
 B = np.random.rand(N, N).astype(np.float32)
-
-
-
-for i in range(100):
+print('\nCPU testing: ')
+SUM = 0
+iterations = 10
+for i in range(iterations):
     flop = 2*N*N*N
     start = time.monotonic()
     # N^2
     C = A @ B
     end = time.monotonic()
     flops = flop / (end - start)
-    print(f"{flops / 1e9:,.2f} GFLOPS")
+    SUM += flops / 1e12
+    print(f"{flops / 1e12:,.2f} GFLOPS")
+print(f"average: {SUM/iterations:,.2f} GFLOPS")
